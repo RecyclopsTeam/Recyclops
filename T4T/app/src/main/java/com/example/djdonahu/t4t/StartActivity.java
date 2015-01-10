2e95bcdad6c6f3615655af652cac5ca14b0ae6c2
Derek Donahue <propolandante@gmail.com>
2015-01-10 13:07:36 -0800

    Created StartActivity with buttons that point to ReferenceActivity (for now, they will soon point to the correct activities). Removed gradle.xml from github, it should no longer be tracked. Created ScanActivity, which will be used to display UPC information after the external scanner has determined the product's UPC.

--- T4T/app/src/main/java/com/example/djdonahu/t4t/StartActivity.java
+++ T4T/app/src/main/java/com/example/djdonahu/t4t/StartActivity.java
@@ -1,18 +1,24 @@
 package com.example.djdonahu.t4t;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
 
 
 public class StartActivity extends ActionBarActivity {
+
+    // initialize View variables (TextViews, ListViews, Buttons, etc)
+    private Button scanButton;
+    private Button statsButton;
+    private Button refButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,57 @@ public class StartActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        // Put all code below this stuff^^^
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //point our View variables to the id that they correspond to
+        scanButton = (Button) findViewById(R.id.scan_button);
+        statsButton = (Button) findViewById(R.id.stats_button);
+        refButton = (Button) findViewById(R.id.ref_button);
+
+        if(scanButton == null)
+        {
+            Log.d("NPE_Alert", "scanButton is null");
+        }
+
+
+        //define the behaviors for when each button is clicked.
+        scanButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(StartActivity.this, ReferenceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        statsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(StartActivity.this, ReferenceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        refButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(StartActivity.this, ReferenceActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
