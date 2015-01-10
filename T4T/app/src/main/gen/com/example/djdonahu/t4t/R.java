7ea08da99dc0f410122fe5b629a9feddbddcd744
Jake Cohen <jamcohen@ucsc.edu>
2015-01-10 12:25:10 -0800

    Added ReferenceActivity

---
+++ T4T/app/src/main/gen/com/example/djdonahu/t4t/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.djdonahu.t4t;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
