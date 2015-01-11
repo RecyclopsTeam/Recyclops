d9c3c86f2467e57227fecc5f1c09d1e675f05ad8
Derek Donahue <propolandante@gmail.com>
2015-01-11 01:56:33 -0800

    outpan didn't pan out. Moving on to parse

--- T4T/app/src/main/java/com/example/djdonahu/t4t/RecyclopsApplication.java
+++ T4T/app/src/main/java/com/example/djdonahu/t4t/RecyclopsApplication.java
@@ -3,6 +3,9 @@ package com.example.djdonahu.t4t;
 import android.app.Application;
 import android.content.Context;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 public class RecyclopsApplication extends Application {
 
     private static Context c;
@@ -16,6 +19,15 @@ public class RecyclopsApplication extends Application {
         c = getApplicationContext();
         // Initialize shared prefs with application context
         SavedPreferences.getInstance(getApplicationContext());
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "QIMMVQrjWNYfbCNfoXEsIU8w3Dyw1E0FlwBtnL7v", "X4PBoUvAeNYkzW1Ihh4nUYLO0tud8zD1j0qRQQOV");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
     }
 
     public static Context getContext() {
