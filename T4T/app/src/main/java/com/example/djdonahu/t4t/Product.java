5ed95ba51b82abb2fd4ea79c6013fd0a40752570
Chris Dilday <seesevn@cox.net>
2015-01-10 18:54:49 -0800

    Added Saved Preferences and updated Product class with info fields

--- T4T/app/src/main/java/com/example/djdonahu/t4t/Product.java
+++ T4T/app/src/main/java/com/example/djdonahu/t4t/Product.java
@@ -1,5 +1,25 @@
 package com.example.djdonahu.t4t;
 
 
+import com.google.gson.annotations.SerializedName;
+
 public class Product {
+
+    @SerializedName("name")
+    public String product_name;
+
+    @SerializedName("packaging_material")
+    public String packaging_material;
+
+    @SerializedName("packaging_size")
+    public String packaging_size;
+
+    @SerializedName("contents_material")
+    public String contents_material;
+
+    @SerializedName("contents_size")
+    public String contents_size;
+
+    @SerializedName("barcode")
+    public String upc;
 }
