85463bdcacb3b01571f8d274a4e66c4f0da36350
Jordan Sommers <jtsommer@ucsc.edu>
2015-01-10 18:11:47 -0800

    First pass of barcode scanner

--- T4T/app/src/main/java/com/example/djdonahu/t4t/ScanActivity.java
+++ T4T/app/src/main/java/com/example/djdonahu/t4t/ScanActivity.java
@@ -1,18 +1,25 @@
 package com.example.djdonahu.t4t;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.*;
 
 
 public class ScanActivity extends ActionBarActivity {
+
+    private TextView scanResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +30,46 @@ public class ScanActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        Intent intent = getIntent();
+        boolean shouldScan = intent.getBooleanExtra("startScanning", false);
+        intent.putExtra("startScanning", false);
+
+        // Don't accidentally get stuck in a scanning loop, only scan on first creation
+        if (shouldScan) {
+            Log.d("T4T", "Launching scan in create");
+            IntentIntegrator integrator = new IntentIntegrator(this);
+            integrator.initiateScan();
+        }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        scanResults = (TextView) findViewById(R.id.scanResult);
+    }
+
+    // Returning from scan app
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        Log.d("T4T", "Scan activity result");
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            // handle scan result
+            if (scanResults != null) {
+                scanResults.setText(scanResult.toString());
+            }
+        }
+    }
+
+    //
+    // Buttons
+    //
+
+    // Scan again
+    public void scanAgain (View v) {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
