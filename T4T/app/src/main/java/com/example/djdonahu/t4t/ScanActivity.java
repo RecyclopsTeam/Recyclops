f9a8d59082cb6aad8e8c591ca184839bf2306bbe
Jordan Sommers <jtsommer@ucsc.edu>
2015-01-11 11:07:31 -0800

    Actually store things, fix crashes

--- T4T/app/src/main/java/com/example/djdonahu/t4t/ScanActivity.java
+++ T4T/app/src/main/java/com/example/djdonahu/t4t/ScanActivity.java
@@ -82,7 +82,7 @@ public class ScanActivity extends ActionBarActivity {
 
     }
 
-    private void loadProductView(Product product)
+    private void loadProductView(final Product product)
     {
         if (product == null) return;
 
@@ -110,8 +110,8 @@ public class ScanActivity extends ActionBarActivity {
             @Override
             public void onClick(View v)
             {
-                Toast toast = Toast.makeText(getApplicationContext(), "Item stored, returning to scanner", Toast.LENGTH_SHORT);
-                toast.show();
+                SavedPreferences.getInstance().addProduct(product);
+                scanAgain(v);
             }
         });
 
@@ -120,8 +120,6 @@ public class ScanActivity extends ActionBarActivity {
             @Override
             public void onClick(View v)
             {
-                Toast toast = Toast.makeText(getApplicationContext(), "Item discarded, returning to scanner", Toast.LENGTH_SHORT);
-                toast.show();
                 scanAgain(v);
             }
         });
@@ -131,6 +129,7 @@ public class ScanActivity extends ActionBarActivity {
             @Override
             public void onClick(View v)
             {
+                SavedPreferences.getInstance().addProduct(product);
                 Intent intent = new Intent(ScanActivity.this, StartActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
