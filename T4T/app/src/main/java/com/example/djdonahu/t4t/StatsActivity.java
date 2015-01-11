1a2126521a05fa3cfda43583379d3978a6a7a0fd
Jordan Sommers <jtsommer@ucsc.edu>
2015-01-11 11:36:39 -0800

    Fix crash on zoom in stats

--- T4T/app/src/main/java/com/example/djdonahu/t4t/StatsActivity.java
+++ T4T/app/src/main/java/com/example/djdonahu/t4t/StatsActivity.java
@@ -125,11 +125,17 @@ public class StatsActivity extends ActionBarActivity {
             @Override
             public String formatLabel(double value, boolean isValueX) {
                 if (isValueX) {
-                    // show normal x values
-                    return graphStats.labels[(int)value];
+                    int v = (int)Math.round(value);
+                    if (v >= 0 && v < graphStats.labels.length) {
+                        // show normal x values
+                        return graphStats.labels[v];
+                    } else {
+                        // Out of bounds
+                        return "";
+                    }
                 } else {
                     // show currency for y values
-                    return super.formatLabel(value, isValueX) + " €";
+                    return super.formatLabel(value, isValueX);
                 }
             }
         });
